33c82290c190c39302b24c6fa3f42c36f1969fb4
MohammedAlMaqbli <Mohammed.AlMaqbli@gmail.com>
2019-09-30 20:10:45 -0700

    Done

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
+import { WSAEISCONN } from "constants";
+
 const testVar = {}
 
 function testFunc() {
   return "hi"
+}
+function superbowlWin(winArray ){
+  let res;
+  res = winArray.find( function(r) { return r.result === "W" });
+  if(typeof res ==="undefined"){return res;}
+  else{
+    return res.year;
+  }
+  
 }
