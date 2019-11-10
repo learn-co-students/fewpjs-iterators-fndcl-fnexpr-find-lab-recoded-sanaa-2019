55e059f7020282dfe3834410649284f6c710564b
Atheer Alaghbari <atheeralaghbari61@gmail.com>
2019-11-10 09:52:27 +0000

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,20 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+const record = [
+  {year: "2018", result: "L"},
+  {year: "2017", result: "W"},
+  {year: "2016", result: "N/A"}
+
+]
+
+function superbowlWin(record){
+  let result=record.find(e=> e.result==="W")
+  if(result){
+    return result.year;
+  }
+  else
+  {
+    return undefined;
+  }
+}
