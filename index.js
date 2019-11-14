b4d6dddcdb48b73f5b50ac54421bc9aba84a4bed
Aiman Almureish <aiman.almureish@gmail.com>
2019-11-14 17:48:13 +0000

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,28 @@
+
 const testVar = {}
 
 function testFunc() {
   return "hi"
 }
+
+superbowlWin(record)
+
+function superbowlWin(array) {
+  let arr = array.find(function (e) {
+    return e.result === "W"
+
+
+  })
+
+  if (arr != undefined) {
+    console.log(arr.year);
+
+    return arr.year
+  } else {
+    console.log(undefined);
+
+    return undefined
+  }
+
+}
+
