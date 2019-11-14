c14946706414c33a9def124541da9bc69bf89236
Aiman Almureish <aiman.almureish@gmail.com>
2019-11-14 17:47:55 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,3 +3,18 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(array){
+  let arr = array.find(function(e){
+    
+     if(e.result === "W"){
+  return e.result
+}else{
+  return undefined
+}
+    
+})
+
+
+
+}
