954575d66f1af8ebc016e281f99e5ca9235737e8
mustafa-al-shaheri <mustafa.alshaheri@gmail.com>
2019-11-16 12:31:24 -0800

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,35 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+const record = [
+  {
+    year: "2018", 
+    result: "L"
+  },
+  {
+    year: "2017",
+     result: "W"
+    },
+  {
+    year: "2016", 
+    result: "N/A"
+  }
+  
+]
+
+function superbowlWin(record){
+
+let returned=record.find(function(item){
+  return item.result=="W"
+})
+
+if (returned==undefined){
+  return undefined
+}else{
+
+  return returned.year
+}
+
+}
+superbowlWin(record)
