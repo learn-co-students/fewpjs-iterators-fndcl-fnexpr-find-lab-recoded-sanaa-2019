91466673016795c4bbb0645f21473ba73fa5e30e
learn <mohammedazizbuss@gmail.com>
2019-11-15 21:52:10 -0800

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,21 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+const record = [
+  {year: "2018", result: "L"},
+  {year: "2017", result: "W"},
+  {year: "2016", result: "N/A"}
+]
+function superbowlWin(arr){
+  let returned;
+  returned= arr.find(function(item){
+    return item.result=="W";
+  })
+  if(returned ==undefined){
+    return undefined;
+  }
+  else {
+    return returned.year;
+  }
+}
+console.log(superbowlWin(record));
