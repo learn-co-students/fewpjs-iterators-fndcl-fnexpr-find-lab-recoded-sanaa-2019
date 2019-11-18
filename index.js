109d2df08ad3479e9e33ee067baad89c7b057315
Ahmed Hassan <ahmedalhassany1@gmail.com>
2019-11-18 13:11:30 +0000

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,13 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(arr){
+  let found = arr.find(function(item){
+    return item.result === "W";
+  });
+  if(found)
+  return found.year;
+  else
+  return found;
+}
