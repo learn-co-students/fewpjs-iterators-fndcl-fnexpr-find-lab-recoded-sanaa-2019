bc62f22dd83535bba3689c909919eda36311b7a5
Eman Al-zariey <eman_alzariey@yahoo.com>
2019-11-18 08:49:31 +0000

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,14 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+
+function superbowlWin(arrOfObj){
+
+  let foundObj = arrOfObj.find(item =>
+    item.result == "W"
+  )
+  if(foundObj){return foundObj.year}
+  
+}
+
