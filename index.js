8f9edd04b0cc1d12f1dc17b9e60db1b43544f5eb
Sarah Alafif <sar1997yah@gmail.com>
2019-12-05 23:13:03 +0000

    this is it

--- index.js
+++ index.js
@@ -1,12 +1,6 @@
-const testVar = [
-  {year: "2018", result: "L"},
-  {year: "2017", result: "W"},
-  {year: "2016", result: "N/A"}
-  ]
 
-function testFunc() {
-  return "hi"
-}
+
+
 
 function superbowlWin(Array){
   
@@ -19,4 +13,3 @@ function superbowlWin(Array){
   
 }
 
-superbowlWin(testVar)
