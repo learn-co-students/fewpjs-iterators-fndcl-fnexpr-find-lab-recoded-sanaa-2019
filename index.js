c7e4a1984f4f05630e54ac86d06b11aeba9a9c05
Sarah Alafif <sar1997yah@gmail.com>
2019-12-05 23:09:59 +0000

    origin master

--- index.js
+++ index.js
@@ -1,5 +1,22 @@
-const testVar = {}
+const testVar = [
+  {year: "2018", result: "L"},
+  {year: "2017", result: "W"},
+  {year: "2016", result: "N/A"}
+  ]
 
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(Array){
+  
+  const n= Array.find(({ result })=>result==="W")
+  if(typeof n === "undefined"){
+    return undefined;
+  }else{
+    return n.year;
+  }
+  
+}
+
+superbowlWin(testVar)
