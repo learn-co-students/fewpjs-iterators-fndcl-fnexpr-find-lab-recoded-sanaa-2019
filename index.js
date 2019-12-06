732dcd8aee2afb9fd6710bc2ce7ce8a399e2e90d
Firas Al-Azizy <firasazizy614@gmail.com>
2019-12-05 22:41:54 -0800

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,18 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+const record = [
+  {year: "2018", result: "L"},
+  {year: "2017", result: "W"},
+  {year: "2016", result: "N/A"}
+]
+
+
+function superbowlWin(arr){
+  let res_yer
+  res_yer =arr.find( function(f) { return f.result === "W" })
+  if(typeof res_yer ==="undefined"){return res_yer}
+  else{
+    return res_yer.year} 
+}
+superbowlWin(record);
